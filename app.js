f301eefc76b9383a4029e4d5d62fbbdd45e2be1c
QuangNo1Pro <levanquangh7@gmail.com>
2024-11-17 10:25:16 +0700

    Xong phần home và DarkMode

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const Header={
+    props:['isDarkMode'],
     template: `
         <header>
             <div class="mssv">22120296</div>  
             <div class="title">Movies Info</div>
             <div class="switch">
-                <input type="checkbox" id="mode-switch" class="toggle-switch">
+                <input @change="toggleDarkMode" :checked="isDarkMode" type="checkbox" id="mode-switch" class="toggle-switch">
                 <label for="mode-switch"></label>
-                <div class="setting">⚙️<div/>
+                <div class="setting">{{ isDarkMode ? '🌙' : '⚙️' }}<div/>
             </div>
         </header>
   `,
+  methods:{
+    //Sự kiện DarkMode
+    toggleDarkMode(event){
+        const isDarkMode= event.target.checked;
+        this.$emit("toggle-dark-mode", isDarkMode);
+    },
+
+
+  },
 };
 
 const NavBar={
     template:`
         <nav>
-            <button class="home">🏠</button>
+            <button @click="goHome" class="home">🏠</button>
             <div class="search">
                 <input class="input_search" type="search" placeholder="Search">
                 <button class="button_search">Search</button>
@@ -24,6 +34,12 @@ const NavBar={
     `,
 
     methods:{
+        goHome(){
+            this.$emit('go-home');
+
+
+        },
+
 
     },
 };
@@ -54,16 +70,37 @@ const app = Vue.createApp({
     },
     data(){
         return{
+            currentPage:1,
+            isDarkMode:false,
 
         };
     },
     methods:{
+        loadPage()
+        {
+            this.currentPage=1;
+        },
+        toggleDarkMode(isDarkMode){
+            this.isDarkMode=isDarkMode;
+            const body=document.body;
+            if(isDarkMode)
+            {
+                body.classList.add("dark-mode")
+            }
+            else{
+                body.classList.remove("dark-mode")
 
+            }
+
+        },
+    },
+    mounted(){
+        this.loadPage();
     },
 
     template:`
-        <Header/>
-        <NavBar/>
+        <Header @toggle-dark-mode="toggleDarkMode" :isDarkMode="isDarkMode"/ >
+        <NavBar @go-home="loadPage()" />
         <Main/>
         <Footer/>
     `,
